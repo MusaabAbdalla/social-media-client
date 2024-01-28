8e4cadcee921155e76684a778b61711e3874044c
Musaab Abdalla <musabd50118@stud.noroff.no>
2024-01-28 22:59:21 +0100

    adds jest test

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,31 +1,33 @@
-// import { createMockFetch } from "../../../mocks/fetch.mock";
-// import { localStorageMock } from "../../../mocks/localStorage.mock";
-import * as mocks from "../../../mocks";
-import { login } from "./login";
-import { load } from "../../storage";
+import { apiPath } from "../constants.js";
+import { login } from "./login.js";
+import { headers } from "../headers.js";
+import { save } from "../../storage/save.js";
 
-const userResposeClone = () => ({ ...mocks.authResponse });
+global.fetch = jest.fn();
+jest.mock("../../storage/save.js");
+const userData = {
+  email: "fakeuser@noroff.com",
+  password: "122aasdf",
+};
+const { email, password } = userData;
 
-describe("login", () => {
-  beforeEach(() => {
-    global.fetch = mocks.createMockFetch(userResposeClone);
-    global.localStorage = mocks.localStorageMock();
-  });
-  afterEach(() => {
-    global.fetch.mockClear();
-    global.localStorage.clear();
-  });
-  afterEach(() => {
-    global.fetch.mockClear();
-    global.localStorage.clear();
-  });
-  it("Should set the token with a successful login", async () => {
-    // global.fetch = mocks.createMockFetch(mocks.authResponse)
-    await login(mocks.userData.email, mocks.userData.password);
-    expect(load("token")).toBe(mocks.accesToken);
-  });
-  it("should return the user name with successful login", async () => {
-    const result = await login(mocks.userData.email, mocks.userData.password);
-    expect(result.name).toEqual(mocks.userData.name);
+describe("Login Function", () => {
+  it("stores the function in the localStorage", async () => {
+    global.fetch.mockResolvedValue({
+      ok: true,
+      json: async () => ({ accessToken: "ABDDAFDSAFSDAFSDAFSDAFSDAFSDAAF" }),
+    });
+
+    await login(userData.email, userData.password);
+    expect(global.fetch).toHaveBeenCalledWith(`${apiPath}/social/auth/login`, {
+      method: "post",
+      body: JSON.stringify({ email, password }),
+      headers: headers("application/json"),
+    });
+
+    expect(save).toHaveBeenCalledWith(
+      "token",
+      "ABDDAFDSAFSDAFSDAFSDAFSDAFSDAAF",
+    );
   });
 });
